cea0f22091c26c4283b66d28638a6b8e4cdb9561
v1tharr <73645120+v1tharr@users.noreply.github.com>
2026-07-02 05:04:44 +0700

    fix: pep8 style

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from app.database import init_db
 
+
 def create_app():
     app = Flask(__name__)
     init_db(app)
     return app
+
 
 if __name__ == '__main__':
     app = create_app()
